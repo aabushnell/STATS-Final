545682dbfa7330b90a43f02a219ebe317782dd3e
aabushnell <aabushnell@gmail.com>
2018-12-08 18:36:29 -0500

    More integrated code

--- app.R
+++ app.R
@@ -54,15 +54,15 @@ ui <- fluidPage(
                        value = 40)
         ),
         
-        submitButton("Search")
+        actionButton("search",
+                     "Search")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
          uiOutput("word1"),
          uiOutput("word2"),
-         uiOutput("word3"),
-         textOutput("test")
+         uiOutput("word3")
       )
    )
 )
@@ -70,7 +70,7 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-   google_coords <- reactive({
+   google_coords <- eventReactive(input$search, {
      
      if (input$search_meth == 'adr') {
        q <- lookup_coords(input$address, apikey = input$google_key)
@@ -84,43 +84,80 @@ server <- function(input, output) {
     return(q)
    })
   
-   #tweet_results <- reactive({
+   tweet_results <- eventReactive(input$search, {
      
-     #create_token(
-       #app = "PicBook",
-       #consumer_key = input$key,
-       #consumer_secret = input$secret_key,
-       #access_token = input$token,
-       #access_secret = input$secret_token)
+     create_token(
+       app = "PicBook",
+       consumer_key = input$key,
+       consumer_secret = input$secret_key,
+       access_token = input$token,
+       access_secret = input$secret_token)
      
-     #rt <- search_tweets(
-       #"lang:en", geocode = q, n = 100)
+     rt <- search_tweets(
+       "lang:en", geocode = google_coords(), n = 100)
+     print(rt)
      
-     #return(rt)
-   #})
+     return(rt)
+   })
+   
+   words_list <- eventReactive(input$search, {
+    
+     text <- tweet_results() %>%
+       select(screen_name, text)
+     
+     match_words <- function(tweet){
+       matched_values <- sapply(words, grepl, tweet)
+       
+       return(matched_values)
+     }
+     
+     most_common_words <- function(df){
+       text_matches <- data.frame(mapply(match_words, df$text)) 
+       
+       word_occurences <- text_matches %>%
+         cbind(rownames(text_matches)) %>%
+         rename(word = 'rownames(text_matches)') %>%
+         mutate(occurences = rowSums(. == TRUE)) %>%
+         select(word, occurences) %>%
+         arrange(desc(occurences))
+       
+       return(word_occurences)
+     }
+     
+     top_n_words <- function(df, n) {
+       words <- most_common_words(text) %>%
+         select(word)
+       
+       top_words <- list()
+       for (i in 1:n) {
+         top_words[i] <- as.character(words$word)[i]
+       }
+       return(top_words)
+     }
+     
+     print(top_n_words(text, 3))
+     
+     return(top_n_words(text, 3))
+   })
    
    output$word1 <- renderUI({
      
-     tags$img(src = lookup_nth_icon(text, 1))
+     
      
    })
    
    output$word2 <- renderUI({
      
-     tags$img(src = lookup_nth_icon(text, 2))
+     
      
    })
    
    output$word3 <- renderUI({
      
-     tags$img(src = lookup_nth_icon(text, 3))
+     
      
    })
-   
-   output$test <- renderText({
-     
-     google_coords()
-   })
+
 }
 
 # Run the application 
