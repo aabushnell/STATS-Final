6e8dbb5fa82646ed6d9fcb7a0025aff9cfc4b557
aabushnell <aabushnell@gmail.com>
2018-12-09 22:45:28 -0500

    Removed keys

--- API_code.R
+++ API_code.R
@@ -9,17 +9,12 @@ library(magick)
 library(rsvg)
 
 
-GOOGLE_MAPS_KEY <- "AIzaSyBtt_cqXkkEYIewZp5ZYp1fdhUksN92OA8"
+GOOGLE_MAPS_KEY <- ""
 
-q = lookup_coords("paris", apikey = "AIzaSyBtt_cqXkkEYIewZp5ZYp1fdhUksN92OA8")
+q = lookup_coords("paris", apikey = "")
 q
 
-create_token(
-  app = "PicBook",
-  consumer_key = "T3GvwW1WZ0svXvFgjAWPRbIRq",
-  consumer_secret = "DWaSGahKAsDmo2j5TFywRBOgmpdOUo9zmGr3IdTVWyPGewl5X2",
-  access_token = "1061380837659344896-iuoPsbhuXodf9SW86i1GhVJZ7iY8pI",
-  access_secret = "pbuRacyoSRgOQC8QAPYI65ci6MZEBNdtKt2lLwvqC0yTb")
+
 
 rt <- search_tweets(
   "lang:en", geocode = q, n = 100)
@@ -29,7 +24,7 @@ paste("1","mi", sep = "")
 google_api <- "https://maps.googleapis.com/maps/api/geocode/json"
 r <- GET(google_api, query=list(address=address))
 
-nouns_app <- oauth_app("nouns_api", "052ecc4e19c341758fdd1bb618ce6cb4", "b1ca275beca84816ad5c16efe798b4e4")
+nouns_app <- oauth_app("nouns_api", "", "")
 
 get_nouns_api <- function(endpoint, baseurl = "http://api.thenounproject.com/", app = nouns_app) {
   url <- modify_url(baseurl, path = endpoint)
